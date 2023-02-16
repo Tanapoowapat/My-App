8e21f4c1551705be51d9de24959b57f8d564d354
Tanapoowpat <tanapoowapat@gmail.com>
2023-02-17 02:14:37 +0700

    Edit Footer & Add some Main Content

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="flex justify-center p-2 bg-black text-white mt-2">
+    <footer className="flex justify-center p-2 bg-black text-white mt-auto">
       @2023 Tanapoowapat Yosmarn. This website is for educate only
     </footer>
   )
--- src/component/Main_Content.jsx
+++ src/component/Main_Content.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 
 const Main_Content = () => {
   return (
-    <div className="container bg-black text-white mt-2 mb-3">
-      <div class="bg-gray-800 p-2 rounded-md flex justify-center">
-        <div className="bg-gray-500 p-2 rounded-lg  ">
-          <span>Hi I'm Tanapoowapat Yomsarn from Thailand.</span>
+    <div className="container h-screen text-white mt-2 mb-3">
+      <div class="bg-gray-800 p-4 rounded-md flex-col justify-items-center">
+        <div className="bg-gray-600 p-2 rounded-lg flex items-center justify-center md:mx-80">
+          <span>Hi I'm Tanapoowapat from Thailand.</span>
+        </div>
+
+        <div class="mx-3 grid grid-cols-2 gap-4 mt-4">
+          <div className=" justify-self-end p-2">
+            <p className="text-xl font-bold">Tanapoowapat Yomsarn</p>
+            <p className="mt-2">Rookie Web Application developer</p>
+          </div>
+          <div className="p-2">My image</div>
         </div>
       </div>
     </div>
